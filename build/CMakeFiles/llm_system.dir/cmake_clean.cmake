93ad3e7a454f5f5f3b44586de9bdf565cd87d435
王润泽 <wangrunze@wangrunzedeMac-mini.local>
2026-07-07 11:26:11 +0800

    Ignore local build artifacts

--- build/CMakeFiles/llm_system.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libllm_system.pdb"
-  "libllm_system.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/llm_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
